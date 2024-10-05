14368de3a0d6917736e5d4ba4844f0531bca711c
Arthur Axiotes <axiotesarthur@gmail.com>
2024-10-05 14:00:27 -0300

    wip: layout desktop da pagina home

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,7 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ButtonComponent } from '../button/button.component';
-import { BreakpointsService } from '../../service/breakpoints.service';
-import { BreakpointsScreen } from '../../../types/breakpoint-screens.type';
 
 @Component({
   selector: 'app-header',
