315964743df250d56110f49b85c7f8413bedf32e
Arthur Axiotes <axiotesarthur@gmail.com>
2024-10-16 21:06:25 -0300

    feat: barra de navegacao

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,12 +1,18 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { LucideAngularModule, Home, LibraryBig } from 'lucide-angular';
+import { LucideIconData } from 'lucide-angular/icons/types';
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [],
+  imports: [LucideAngularModule, NgFor],
   templateUrl: './navigation.component.html',
-  styleUrl: './navigation.component.scss'
+  styleUrl: './navigation.component.scss',
 })
 export class NavigationComponent {
-
+  public icons: { icon: LucideIconData }[] = [
+    { icon: Home },
+    { icon: LibraryBig },
+  ];
 }
