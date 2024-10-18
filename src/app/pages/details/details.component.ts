21a304a2dd42e6e4d9d96fe550fc0cfb9517926d
Arthur Axiotes <axiotesarthur@gmail.com>
2024-10-18 08:21:50 -0300

    wip: infos chegando no compoente de details

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { AllDetails } from '../../../types/all-details.type';
+import { allDetails } from '../../../data/all-details';
+import { InfoDetails } from '../../../types/info-details.type';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrl: './details.component.scss'
+  styleUrl: './details.component.scss',
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit {
+  @Input() public slug!: string;
+  public allDetails: AllDetails[] = allDetails;
+  public infoDetails!: InfoDetails[];
 
+  ngOnInit(): void {
+    this.allDetails
+      .filter((detail) => detail.slug === this.slug)
+      .map((detail) => (this.infoDetails = detail.infoDetails));
+
+    console.log(this.infoDetails);
+  }
 }
