de3c463f145bab2695218884e1a506bf02859f11
Arthur Axiotes <axiotesarthur@gmail.com>
2024-10-18 10:36:18 -0300

    feat: componente de video

--- src/app/components/step-section/step-section.component.ts
+++ src/app/components/step-section/step-section.component.ts
@@ -1,13 +1,14 @@
-import { NgClass } from '@angular/common';
+import { NgClass, NgFor } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { BookOpen, ChevronUp, LucideAngularModule } from 'lucide-angular';
 import { LucideIconData } from 'lucide-angular/icons/types';
 import { Videos } from '../../../types/videos.type';
+import { VideosComponent } from '../videos/videos.component';
 
 @Component({
   selector: 'app-step-section',
   standalone: true,
-  imports: [LucideAngularModule, NgClass],
+  imports: [LucideAngularModule, NgClass, VideosComponent, NgFor],
   templateUrl: './step-section.component.html',
   styleUrl: './step-section.component.scss',
 })
